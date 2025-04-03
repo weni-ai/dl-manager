8a667c9122780f2ecf7517af65fcbd7dda1c393b
lucaslinhares <lucaslinhares027@gmail.com>
2025-04-03 00:16:17 -0300

    Insert Trace path

--- datalake_manager/server/server.py
+++ datalake_manager/server/server.py
@@ -5,12 +5,17 @@ from google.protobuf.json_format import MessageToDict
 
 import datalake_manager.server.proto.msgs_pb2 as msgs_pb2
 import datalake_manager.server.proto.msgs_pb2_grpc as msgs_pb2_grpc
+import datalake_manager.server.proto.traces_pb2 as traces_pb2
+import datalake_manager.server.proto.traces_pb2_grpc as traces_pb2_grpc
 from datalake_manager.redshift_manager import RedshiftManager
 
 redshift = RedshiftManager()
 
 
-class DatalakeManagerService(msgs_pb2_grpc.DatalakeManagerServiceServicer):
+class DatalakeManagerService(
+    msgs_pb2_grpc.DatalakeManagerServiceServicer,
+    traces_pb2_grpc.DatalakeManagerServiceServicer,
+):
     def InsertData(self, request, context):
         data_dict = MessageToDict(request.data)
 
@@ -25,10 +30,25 @@ class DatalakeManagerService(msgs_pb2_grpc.DatalakeManagerServiceServicer):
             print("Bad things happens with good people:", str(e))
             return msgs_pb2.InsertResponse(status="error")
 
+    def InsertTraceData(self, request, context):
+        data_dict = MessageToDict(request.data)
+        print(f"Received Trace Data: path={request.path}, data={data_dict}")
+
+        try:
+            response = redshift.insert(request.path, data_dict)
+            print("Trace inserted successfully!", response)
+            return traces_pb2.InsertTraceResponse(status="success")
+        except Exception as e:
+            print("Error inserting trace data:", str(e))
+            return traces_pb2.InsertTraceResponse(status="error")
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     msgs_pb2_grpc.add_DatalakeManagerServiceServicer_to_server(
+        DatalakeManagerService(), server
+    )
+    traces_pb2_grpc.add_DatalakeManagerServiceServicer_to_server(
         DatalakeManagerService(), server
     )
     server.add_insecure_port("[::]:50051")
